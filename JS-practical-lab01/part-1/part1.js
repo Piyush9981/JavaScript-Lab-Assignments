f23367850b915c1d6dc56c09f80c13c82d633d81
unknown <2424159@campus.dsvv.in>
2026-08-20 13:42:29 +0530

    Add lab01

---
+++ JS-practical-lab01/part-1/part1.js
@@ -0,0 +1,15 @@
+    console.log("My Name is Piyush");
+    console.log(5+5);
+    //==========Predicted Output============
+    // My Name is Piyush
+    // 10
+
+// Task 1.1 
+    // console.log(My Name is Piyush);
+    // Error Message: Uncaught SyntaxError: missing ) after argument list
+
+    console.log("My Name is Piyush"); //Fixed Syntax
+
+//  Tasl 1.2 
+    console.log("test");
+    console.log("Test")
---
+++ JS-practical-lab01/part-3/part3.js
@@ -0,0 +1,18 @@
+let city = "Haridwar"; // String
+let population = 300000; // Number
+let isCapital = false; // Boolean
+let mayor; // Undefined
+let previousMayor = null; // Null
+
+console.log(typeof city, typeof population, typeof isCapital, typeof mayor, typeof previousMayor);
+
+// Task 3.1
+    let name = "Piyush" //String
+    let age = 20; //Number
+    let isMarried = false //Boolean
+    let course; //Undefine
+    let job = null; //Null
+    let speedOfLight = 3n //bigint
+    let studentId = Symbol("SECRET_12345") //symbol
+
+    console.log(name, ":", typeof name,"|", age, ":", typeof age,"|", isMarried, ":", typeof isMarried,"|",course, ":", typeof course,"|",job, ":", typeof job,"|",speedOfLight, ":", typeof speedOfLight,"|",studentId, ":", typeof studentId);
